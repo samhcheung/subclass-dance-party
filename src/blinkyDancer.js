80792f38871a951947b53c7759bef863b508125e
Samuel Cheung <samhcheung@Samuels-MacBook-Air.local>
2016-08-01 14:37:03 -0700

    Change to pseudoclassical, but dance test still fails

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,23 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  //console.log(this.oldStep);
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.oldStep = Dancer.prototype.step;
+  console.log(this.oldStep);
+};
+
+  BlinkyDancer.prototype = Object.create(Dancer.prototype);
+  BlinkyDancer.prototype.constructor = BlinkyDancer;
+
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
+  BlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
+    //console.log(this.oldStep);
+    this.oldStep.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
+    this.$node.toggle();
   };
-
-  return blinkyDancer;
-};
